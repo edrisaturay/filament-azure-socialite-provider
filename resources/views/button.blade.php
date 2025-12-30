ffe45ae603da8eb48a9a5bace56edd4a043cd75c
Edrisa Turay <edrisaa.turay@gmail.com>
2025-12-30 04:37:16 +0100

    Simplify button to match Filament sign-in button style
    - Remove Microsoft icon/SVG
    - Use Filament button classes (fi-btn, fi-btn-color-primary, fi-btn-size-lg)
    - Remove custom styling and borders
    - Simple text-only button matching Filament's native button style

--- resources/views/button.blade.php
+++ resources/views/button.blade.php
@@ -30,18 +30,12 @@
     }
 @endphp
 
-<div class="fi-azure-socialite-button mt-4">
+<div class="mt-4">
     <a
         href="{{ route($routeName) }}"
-        class="fi-btn fi-btn-color-gray fi-btn-size-lg fi-btn-outline flex w-full items-center justify-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2 font-semibold text-gray-700 shadow-sm transition duration-75 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700"
+        class="fi-btn fi-btn-color-primary fi-btn-size-lg w-full justify-center"
     >
-        <svg class="h-5 w-5" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0h11.377v11.372H0z" fill="#f25022"/>
-            <path d="M12.623 0H24v11.372H12.623z" fill="#00a4ef"/>
-            <path d="M0 12.628h11.377V24H0z" fill="#7fba00"/>
-            <path d="M12.623 12.628H24V24H12.623z" fill="#ffb900"/>
-        </svg>
-        <span>{{ $buttonLabel }}</span>
+        {{ $buttonLabel }}
     </a>
 </div>
 
